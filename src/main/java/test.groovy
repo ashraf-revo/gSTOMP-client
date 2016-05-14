b0f003902638c18ca751b7842e3daab18ef06503
ashraf <ashraf1abdelrasool@gmail.com>
2016-05-15 00:00:32 +0200

    d

--- src/main/java/test.groovy
+++ src/main/java/test.groovy
@@ -6,9 +6,9 @@ class test {
     static private Gson gson = new Gson();
 
     public static void main(String[] args) {
-        String key = "fc7578b2-6318-4cdb-9137-cde68359d0b6";
+        String key = "94cc9199-7e04-4225-b226-3053bcf0aca0";
 
-        StompClient stompClient = StompClient.overSockJs(new URL("http://localhost:8080/hello/"), null, ['X-API-KEY': '1234567', "Authorization": "bearer " + key])
+        StompClient stompClient = StompClient.overSockJs(new URL("https://shared.mybluemix.net/hello/"), null, ['X-API-KEY': '1234567', "Authorization": "bearer " + key])
         stompClient.setAutoReconnect(true)
         stompClient.connect()
 
@@ -19,6 +19,6 @@ class test {
         }
 
         stompClient.send("/app/hello", ["content-type":"application/json"], gson.toJson(new Message(" welcome")))
-        Thread.sleep(1000)
+        Thread.sleep(8000)
     }
 }
